2af85ffaa9a9b13a8196d4bb19307de7145d730f
theprophecy <thearyanahmed@gmail.com>
2020-05-17 23:20:24 +0600

    added json serializer

--- shortner/serializer/json/serializer.go
+++ shortner/serializer/json/serializer.go
@@ -3,9 +3,25 @@ package json
 import (
 	"encoding/json"
 	"github.com/pkg/errors"
-	// "github.com/thearyanahmed/url-shortner/shortner"
+	"github.com/thearyanahmed/url-shortner/shortner"
 )
 
 type Redirect struct{}
 
-// func (r *Redirect) Decode(input []byte) (* shortner.Redirect)
+func (r *Redirect) Decode(input []byte) (* shortner.Redirect, error) {
+	redirect := &shortner.Redirect{}
+
+	if err := json.Unmarshal(input,redirect); err != nil {
+		return nil, errors.Wrap("serializer.Redirect.Decode")
+	}
+
+	return redirect, nil
+}
+
+func (r *Redirect) Encode(input *Redirect) ([]byte, error) {
+	bytes, err := json.Marshal(input) ; err != nil {
+		return nil, errors.Wrap("serializer.Redirect.Endcode")
+	}
+
+	return bytes, nil
+}
