67487d2294ae11acdb43766638ec4dae757adac0
theprophecy <thearyanahmed@gmail.com>
2020-05-17 22:56:48 +0600

    updated mod

---
+++ shortner/serializer/json/serializer.go
@@ -0,0 +1,11 @@
+package json
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	// "github.com/thearyanahmed/url-shortner/shortner"
+)
+
+type Redirect struct{}
+
+// func (r *Redirect) Decode(input []byte) (* shortner.Redirect)
