e3f6d7453ce45d6a9c7d31e95571da0bae427f11
theprophecy <thearyanahmed@gmail.com>
2020-05-17 23:26:11 +0600

    added msgpack serializer

---
+++ shortner/serializer/msgpack/serializer.go
@@ -0,0 +1,27 @@
+package msgpack
+
+import (
+	"github.com/vmihailenco/msgpack"
+	"github.com/pkg/errors"
+	"github.com/thearyanahmed/url-shortner/shortner"
+)
+
+type Redirect struct{}
+
+func (r *Redirect) Decode(input []byte) (* shortner.Redirect, error) {
+	redirect := &shortner.Redirect{}
+
+	if err := msgpack.Unmarshal(input,redirect); err != nil {
+		return nil, errors.Wrap("serializer.Redirect.Decode")
+	}
+
+	return redirect, nil
+}
+
+func (r *Redirect) Encode(input *Redirect) ([]byte, error) {
+	bytes, err := msgpack.Marshal(input) ; err != nil {
+		return nil, errors.Wrap("serializer.Redirect.Endcode")
+	}
+
+	return bytes, nil
+}
